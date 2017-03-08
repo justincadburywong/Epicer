a9861702ea85e924e53d3c78f916d58c9e01081b
Justincadburywong <justincadburywong@gmail.com>
2017-03-08 00:08:50 -0800

    add lots of validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
 
   validates :first_name, :last_name, :email, :password, presence: true
+  validates :password, length: { minimum: 6 }
+  validates_associated :recipes
+  validates :email, uniqueness: true
+            format { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+            message: "Incorrect email format"}
 end
