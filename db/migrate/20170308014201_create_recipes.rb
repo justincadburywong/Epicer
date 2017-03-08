35b0541a9f5369b24cc4bc0baf5e695789beba6a
Justincadburywong <justincadburywong@gmail.com>
2017-03-07 23:28:02 -0800

    update recipe migration with default values

--- db/migrate/20170308014201_create_recipes.rb
+++ db/migrate/20170308014201_create_recipes.rb
@@ -3,77 +3,77 @@ class CreateRecipes < ActiveRecord::Migration[5.0]
     create_table :recipes do |t|
       t.string :name
       t.string :description
-      t.string :source
+      t.string :source, :default => nil
       t.string :category
-      t.string :prep_time
-      t.string :servings
-      t.string :cals_serving
-      t.binary :attachments
-      t.string :notes
+      t.string :prep_time, :default => nil
+      t.string :servings, :default => nil
+      t.string :cals_serving, :default => nil
+      t.binary :attachments, :default => nil
+      t.string :notes, :default => nil
       t.string :ingredient1
-      t.string :ingredient2
-      t.string :ingredient3
-      t.string :ingredient4
-      t.string :ingredient5
-      t.string :ingredient6
-      t.string :ingredient7
-      t.string :ingredient8
-      t.string :ingredient9
-      t.string :ingredient10
-      t.string :ingredient11
-      t.string :ingredient12
-      t.string :ingredient13
-      t.string :ingredient14
-      t.string :ingredient15
-      t.string :ingredient16
-      t.string :ingredient17
-      t.string :ingredient18
-      t.string :ingredient19
-      t.string :ingredient20
-      t.string :ingredient21
-      t.string :ingredient22
+      t.string :ingredient2, :default => nil
+      t.string :ingredient3, :default => nil
+      t.string :ingredient4, :default => nil
+      t.string :ingredient5, :default => nil
+      t.string :ingredient6, :default => nil
+      t.string :ingredient7, :default => nil
+      t.string :ingredient8, :default => nil
+      t.string :ingredient9, :default => nil
+      t.string :ingredient10, :default => nil
+      t.string :ingredient11, :default => nil
+      t.string :ingredient12, :default => nil
+      t.string :ingredient13, :default => nil
+      t.string :ingredient14, :default => nil
+      t.string :ingredient15, :default => nil
+      t.string :ingredient16, :default => nil
+      t.string :ingredient17, :default => nil
+      t.string :ingredient18, :default => nil
+      t.string :ingredient19, :default => nil
+      t.string :ingredient20, :default => nil
+      t.string :ingredient21, :default => nil
+      t.string :ingredient22, :default => nil
       t.integer :quantity1
-      t.integer :quantity2
-      t.integer :quantity3
-      t.integer :quantity4
-      t.integer :quantity5
-      t.integer :quantity6
-      t.integer :quantity7
-      t.integer :quantity8
-      t.integer :quantity9
-      t.integer :quantity10
-      t.integer :quantity11
-      t.integer :quantity12
-      t.integer :quantity13
-      t.integer :quantity14
-      t.integer :quantity15
-      t.integer :quantity16
-      t.integer :quantity17
-      t.integer :quantity18
-      t.integer :quantity19
-      t.integer :quantity20
-      t.integer :quantity21
-      t.integer :quantity22
+      t.integer :quantity2, :default => nil
+      t.integer :quantity3, :default => nil
+      t.integer :quantity4, :default => nil
+      t.integer :quantity5, :default => nil
+      t.integer :quantity6, :default => nil
+      t.integer :quantity7, :default => nil
+      t.integer :quantity8, :default => nil
+      t.integer :quantity9, :default => nil
+      t.integer :quantity10, :default => nil
+      t.integer :quantity11, :default => nil
+      t.integer :quantity12, :default => nil
+      t.integer :quantity13, :default => nil
+      t.integer :quantity14, :default => nil
+      t.integer :quantity15, :default => nil
+      t.integer :quantity16, :default => nil
+      t.integer :quantity17, :default => nil
+      t.integer :quantity18, :default => nil
+      t.integer :quantity19, :default => nil
+      t.integer :quantity20, :default => nil
+      t.integer :quantity21, :default => nil
+      t.integer :quantity22, :default => nil
       t.text :instruction1
-      t.text :instruction2
-      t.text :instruction3
-      t.text :instruction4
-      t.text :instruction5
-      t.text :instruction6
-      t.text :instruction7
-      t.text :instruction8
-      t.text :instruction9
-      t.text :instruction10
-      t.text :instruction11
-      t.text :instruction12
-      t.text :instruction13
-      t.text :instruction14
-      t.text :instruction15
-      t.text :instruction16
-      t.text :instruction17
-      t.text :instruction18
-      t.text :instruction19
-      t.text :instruction20
+      t.text :instruction2, :default => nil
+      t.text :instruction3, :default => nil
+      t.text :instruction4, :default => nil
+      t.text :instruction5, :default => nil
+      t.text :instruction6, :default => nil
+      t.text :instruction7, :default => nil
+      t.text :instruction8, :default => nil
+      t.text :instruction9, :default => nil
+      t.text :instruction10, :default => nil
+      t.text :instruction11, :default => nil
+      t.text :instruction12, :default => nil
+      t.text :instruction13, :default => nil
+      t.text :instruction14, :default => nil
+      t.text :instruction15, :default => nil
+      t.text :instruction16, :default => nil
+      t.text :instruction17, :default => nil
+      t.text :instruction18, :default => nil
+      t.text :instruction19, :default => nil
+      t.text :instruction20, :default => nil
 
       t.timestamps
     end
