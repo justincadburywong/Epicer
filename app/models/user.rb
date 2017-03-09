847f29e52b7f66a857808191ece2d6b40fe73396
Justincadburywong <justincadburywong@gmail.com>
2017-03-08 18:24:53 -0800

    use devise as user driver

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
-  # has_secure_password
-  # has_many :recipes, dependent: :destroy
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :recipes, dependent: :destroy
 
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, :password, presence: true
   validates :password, length: { minimum: 6 }
   validates_associated :recipes
   validates :email, uniqueness: true,
-            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
-            message: "Incorrect email format" }
+           format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+           message: "Incorrect email format" }
 end
